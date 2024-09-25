4c49f0e2f09d3b567922c9aa0c7a7c61a82f2129
Renatowsl <renatodaniel.cb@gmail.com>
2024-09-24 21:54:12 -0500

    modelo difunto

--- difunto/models.py
+++ difunto/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Create difunto
+class Difunto(models.Model):
+    names = models.CharField( max_length=200, verbose_name='nombres')
+    last_names = models.CharField( max_length=200, verbose_name='apellidos')
+    idNumber = models.CharField(max_length=10, unique=True, verbose_name='cedula')
+    requestNumber = models.CharField(max_length=20, unique=True, verbose_name='solicitud')
+    loadDate = models.mDateTimeField(auto_now_add=True, verbose_name='creacion')
+    updateDate = models.mDateTimeField(auto_now=True, verbose_name='actualizacion')
+    decription = models.TextField(max_length=300, blank=True, null=True, verbose_name='observaciones')
+    tumba = models.ForeignKey(Tumba, related_name='difunto', on_delete=models.DO_NOTHING) 
+    deudo = models.ForeignKey(Deudo, related_name='difunto', on_delete=models.DO_NOTHING) 
+
+# Create deudo
+class Deudo(models.Model):
+    names = models.CharField( max_length=200, verbose_name='nombres')
+    last_names = models.CharField( max_length=200, verbose_name='apellidos')
+    idNumber = models.CharField(max_length=10, verbose_name='cedula')
+    phoneNumber = models.CharField(max_length=15, verbose_name='telefono')
+    email = models.EmailField( verbose_name='email')
+    address = models.CharField(max_length=100, blank=True, null=True, verbose_name='direccion')
+    loadDate = models.mDateTimeField(auto_now_add=True, verbose_name='creacion')
+    updateDate = models.mDateTimeField(auto_now=True, verbose_name='actualizacion')
+    decription = models.TextField(max_length=300, blank=True, null=True, verbose_name='observaciones')
+
--- tumba/models.py
+++ tumba/models.py
@@ -2,26 +2,29 @@ from django.db import models
 
 # modelo lote
 class Lote (models.Model):
-    name=models.CharField(max_length=3, verbose_name="lote")
-    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name="lugar")
-    loadDate=models.mDateTimeField(auto_now_add=True)
-    updateDate=models.mDateTimeField(auto_now=True)
+    blockName=models.CharField(max_length=3, verbose_name='lote')
+    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name='lugar')
+    loadDate=models.mDateTimeField(auto_now_add=True, verbose_name='creacion')
+    updateDate=models.mDateTimeField(auto_now=True, verbose_name='actualizacion')
 
+# modelo tumba
 class Tumba (models.Model):
     TIPO_NICHO_CHOICES[
         ('T','Tumba de tierra'),
         ('E','Tumba extramuros'),
     ]
-    numero=models.IntegerField(max_length=4)
-    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name="pocision")
-    tipo_nicho=models.CharField(max_length=2, choices= TIPO_NICHO_CHOICES)
-    loadDate=models.mDateTimeField(auto_now_add=True)
-    updateDate=models.mDateTimeField(auto_now=True)
-    name=models.ForeignKey(Lote, on_delete=models.CASCADE) 
+    nicheNumber=models.IntegerField(max_length=4, verbose_name='tumba')
+    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name='pocision')
+    nicheType=models.CharField(max_length=2, choices= TIPO_NICHO_CHOICES, verbose_name='tipo')
+    loadDate=models.mDateTimeField(auto_now_add=True, verbose_name='creacion')
+    updateDate=models.mDateTimeField(auto_now=True, verbose_name='actualizacion')
+    name=models.ForeignKey(Lote, related_name='tumba', on_delete=models.CASCADE) 
 
+# modelo Disponibilidad
 class DisponibleTumba (models.Model):
-    startDate=models.DateTimeField()
-    endDate=models.DateTimeField()
-    loadDate=models.mDateTimeField(auto_now_add=True)
-    updateDate=models.mDateTimeField(auto_now=True)
-    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name="observaciones")
+    startDate=models.DateTimeField(verbose_name='inicio')
+    endDate=models.DateTimeField(verbose_name='vence')
+    loadDate=models.mDateTimeField(auto_now_add=True, verbose_name='creacion')
+    updateDate=models.mDateTimeField(auto_now=True, verbose_name='actualizacion')
+    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name='observaciones')
+    number=models.ForeignKey(Tumba, related_name='disponibleTumba', on_delete=models..DO_NOTHING) 
