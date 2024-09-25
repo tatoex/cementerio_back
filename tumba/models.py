f8bc70b7df78f04b6a1ced90d58fcb5da2276174
Renatowsl <renatodaniel.cb@gmail.com>
2024-09-24 20:21:33 -0500

    modelo tumba

--- tumba/models.py
+++ tumba/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+# modelo lote
+class Lote (models.Model):
+    name=models.CharField(max_length=3, verbose_name="lote")
+    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name="lugar")
+    loadDate=models.mDateTimeField(auto_now_add=True)
+    updateDate=models.mDateTimeField(auto_now=True)
+
+class Tumba (models.Model):
+    TIPO_NICHO_CHOICES[
+        ('T','Tumba de tierra'),
+        ('E','Tumba extramuros'),
+    ]
+    numero=models.IntegerField(max_length=4)
+    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name="pocision")
+    tipo_nicho=models.CharField(max_length=2, choices= TIPO_NICHO_CHOICES)
+    loadDate=models.mDateTimeField(auto_now_add=True)
+    updateDate=models.mDateTimeField(auto_now=True)
+    name=models.ForeignKey(Lote, on_delete=models.CASCADE) 
+
+class DisponibleTumba (models.Model):
+    startDate=models.DateTimeField()
+    endDate=models.DateTimeField()
+    loadDate=models.mDateTimeField(auto_now_add=True)
+    updateDate=models.mDateTimeField(auto_now=True)
+    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name="observaciones")
