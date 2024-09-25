11258cfc76dccab6fcf2e75a5f0e51c9e564a9f4
Renatowsl <renatodaniel.cb@gmail.com>
2024-09-24 22:51:12 -0500

    modelo servicio

--- difunto/models.py
+++ difunto/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tumba.models import Tumba
 
 # Create difunto
 class Difunto(models.Model):
@@ -6,11 +7,11 @@ class Difunto(models.Model):
     last_names = models.CharField( max_length=200, verbose_name='apellidos')
     idNumber = models.CharField(max_length=10, unique=True, verbose_name='cedula')
     requestNumber = models.CharField(max_length=20, unique=True, verbose_name='solicitud')
-    loadDate = models.mDateTimeField(auto_now_add=True, verbose_name='creacion')
-    updateDate = models.mDateTimeField(auto_now=True, verbose_name='actualizacion')
+    loadDate = models.DateTimeField(auto_now_add=True, verbose_name='creacion')
+    updateDate = models.DateTimeField(auto_now=True, verbose_name='actualizacion')
     decription = models.TextField(max_length=300, blank=True, null=True, verbose_name='observaciones')
-    tumba = models.ForeignKey(Tumba, related_name='difunto', on_delete=models.DO_NOTHING) 
-    deudo = models.ForeignKey(Deudo, related_name='difunto', on_delete=models.DO_NOTHING) 
+    tumba = models.ForeignKey(Tumba, related_name='difuntoTumba', on_delete=models.DO_NOTHING) 
+    deudo = models.ForeignKey(Deudo, related_name='difuntoDeudo', on_delete=models.DO_NOTHING) 
 
 # Create deudo
 class Deudo(models.Model):
@@ -20,7 +21,7 @@ class Deudo(models.Model):
     phoneNumber = models.CharField(max_length=15, verbose_name='telefono')
     email = models.EmailField( verbose_name='email')
     address = models.CharField(max_length=100, blank=True, null=True, verbose_name='direccion')
-    loadDate = models.mDateTimeField(auto_now_add=True, verbose_name='creacion')
-    updateDate = models.mDateTimeField(auto_now=True, verbose_name='actualizacion')
+    loadDate = models.DateTimeField(auto_now_add=True, verbose_name='creacion')
+    updateDate = models.DateTimeField(auto_now=True, verbose_name='actualizacion')
     decription = models.TextField(max_length=300, blank=True, null=True, verbose_name='observaciones')
 
--- tumba/models.py
+++ tumba/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # modelo lote
 class Lote (models.Model):
-    blockName=models.CharField(max_length=3, verbose_name='lote')
-    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name='lugar')
-    loadDate=models.mDateTimeField(auto_now_add=True, verbose_name='creacion')
-    updateDate=models.mDateTimeField(auto_now=True, verbose_name='actualizacion')
+    blockName = models.CharField(max_length=3, verbose_name='lote')
+    decription = models.TextField(max_length=300, blank=True, null=True, verbose_name='lugar')
+    loadDate = models.DateTimeField(auto_now_add=True, verbose_name='creacion')
+    updateDate = models.DateTimeField(auto_now=True, verbose_name='actualizacion')
 
 # modelo tumba
 class Tumba (models.Model):
@@ -13,18 +13,18 @@ class Tumba (models.Model):
         ('T','Tumba de tierra'),
         ('E','Tumba extramuros'),
     ]
-    nicheNumber=models.IntegerField(max_length=4, verbose_name='tumba')
-    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name='pocision')
-    nicheType=models.CharField(max_length=2, choices= TIPO_NICHO_CHOICES, verbose_name='tipo')
-    loadDate=models.mDateTimeField(auto_now_add=True, verbose_name='creacion')
-    updateDate=models.mDateTimeField(auto_now=True, verbose_name='actualizacion')
-    name=models.ForeignKey(Lote, related_name='tumba', on_delete=models.CASCADE) 
+    nicheNumber = models.IntegerField(max_length=4, verbose_name='tumba')
+    decription = models.TextField(max_length=300, blank=True, null=True, verbose_name='pocision')
+    nicheType = models.CharField(max_length=2, choices= TIPO_NICHO_CHOICES, verbose_name='tipo')
+    loadDate = models.DateTimeField(auto_now_add=True, verbose_name='creacion')
+    updateDate = models.DateTimeField(auto_now=True, verbose_name='actualizacion')
+    nameLote = models.ForeignKey(Lote, related_name='tumbaLote', on_delete=models.CASCADE) 
 
 # modelo Disponibilidad
 class DisponibleTumba (models.Model):
-    startDate=models.DateTimeField(verbose_name='inicio')
-    endDate=models.DateTimeField(verbose_name='vence')
-    loadDate=models.mDateTimeField(auto_now_add=True, verbose_name='creacion')
-    updateDate=models.mDateTimeField(auto_now=True, verbose_name='actualizacion')
-    decription=models.TextField(max_length=300, blank=True, null=True, verbose_name='observaciones')
-    number=models.ForeignKey(Tumba, related_name='disponibleTumba', on_delete=models..DO_NOTHING) 
+    startDate = models.DateTimeField(verbose_name='inicio')
+    endDate = models.DateTimeField(verbose_name='vence')
+    loadDate = models.DateTimeField(auto_now_add=True, verbose_name='creacion')
+    updateDate = models.DateTimeField(auto_now=True, verbose_name='actualizacion')
+    decription = models.TextField(max_length=300, blank=True, null=True, verbose_name='observaciones')
+    numberTumba = models.ForeignKey(Tumba, related_name='disponibleTumba', on_delete=models.DO_NOTHING) 
