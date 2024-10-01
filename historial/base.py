6e27fee19167a54aaf840950b3a901cbbd49b2fd
Renatowsl <renatodaniel.cb@gmail.com>
2024-09-30 20:54:10 -0500

    adaptacion de las funcines historial

--- historial/base.py
+++ historial/base.py
@@ -1,7 +1,7 @@
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .utils import consultar_historial, obtener_difernecia, restaurar_version
+from .utils import consultar_historial, obtener_historial_limitado, compara_varias_versiones, comparar_versiones
 
 class HistoricalQuerySetMixin:
     """clase para manejar la obtencionde datos del historial de cualquier modelo con filtros"""
@@ -34,42 +34,65 @@ class HistoricalActionMixin:
     """clase para manejar acciones: consultas, comparar y restaurar versiones """
     @action (methods=['GET'], detail=False, url_path='historial' )
     def historical(self, request):
-        """devuelve el historial del objeto"""
+        """
+        Devuelve el historial del objeto segun su ID.
+        URL Parameters:
+        -object_id: ID del objeto para obtener su historial
+        -limit:(opcional) Numero de verirsiones a mostrar (por defecto).
+        """
 
         object_id=self.request.query_params.get(f'{self.model._meta.model_name.lower()}_id', None)
+        limit=int(self.request.query_params.get(limit, 5))
         if not object_id:
             return Response({"error":"Debe proporcionar la ID del objeto"}, status=status.HTTP_400_BAD_REQUEST)
-        historial=consultar_historial(self.model,object_id)
+        try:
+            historial=obtener_historial_limitado(self.model, object_id, limit)
+        except ValueError as e:
+            return Response({"error":str(e)},status=status.HTTP_404_NOT_FOUND)
+        #  serializar el historial para devolverlo como respuesta
         serializer=self.get_serializer(historial, many=True)
         return Response(serializer.data)
     
     @action (methods=['GET'], detail=False, url_path='comparar')
-    def comparar(self, request):
-        """compara dos versiones del historial"""
-        version1_id=request.query_params.get('version1_id')
-        version2_id=request.query_params.get('version2_id')
+    def comparar_varias_versiones(self, request):
+        """
+        Compara varias versiones de un objeto en todos los atributos o en un atributo especifico
+        URL Parameters:
+        - object_id_ ID del objeto a comparar
+        - attribute: (opcional) Atributoespecifico a comparar, usa 'all' para comparar todos los atributos.
+        - limit: (opcional) Numero de versiones a considerar (por defecto 5).
+        """
+        object_id=self.request.query_params.get(f'{self.model._meta.model_name.lower()}_id', None)
+        attribute=self.request.query_params.get('attribute', 'all')
+        limit=int(self.request.query_params.get(limit, 5))
 
-        if not version1_id or not version2_id:
+        if not object_id:
             return Response({"error":"Debe proporcionar la ID del objeto"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if version1_id==version2_id:
-            return Response({"error":"No se puede comparar la misma version. Proporcione versiones diferentes"},status=status.HTTP_400_BAD_REQUEST)
-        
         try:
-            version1=self.model.history.get(history_id=version1_id)
-            version2=self.model.history.get(history_id=version2_id)
-        except self.model.history.model.DoesNotExist:
-            return Response({"error":"Una de las versiones no existe"},status=status.HTTP_404_NOT_FOUND)
-        
-        diferencias=obtener_difernecia(version1, version2)
-        return Response({"diferencias":diferencias})
+            # llama a la funcion para comparar varias versiones
+            cambios=compara_varias_versiones(self.model, object_id, attribute, limit)
+        except ValueError as e:
+            return Response({"error":str(e)},status=status.HTTP_404_NOT_FOUND)
+
+        return Response({"cambios":cambios})
     
-    @action (methods=['GET'], detail=False, url_path='restaurar')
+    @action (methods=['POST'], detail=False, url_path='restaurar')
     def restaurar(self,request):
-        """Restaura yb objeto a una version anterior"""
-        version_id=request.data.get('version_id')
+        """
+        Restaura un objeto a una version anterior
+        Body Parameters:
+        - version_id: ID de la version historica que se desea restaurar
+        """
+        version_id=request.data.get('version_id',None)
         if not version_id:
             return Response({"error":"Debe proporcionar la ID del objeto"}, status=status.HTTP_400_BAD_REQUEST)
-        version=self.model.histry.get(hitory_id=version_id)
-        restaurar_version(version)
-        return Response({"mensaje":"Version restaurada exitosamente"})
+        try:
+            # obtener la version historica
+            version=self.model.histry.get(hitory_id=version_id)
+            # restauramos el objeto a la version anterior
+            version.instance.save()
+        except self.model.history.model.DoesNotExist:
+            return Response({"error":"La version especifica no existe"}, status=status.HTTP_404_NOT_FOUND)
+       
+        return Response({"mensaje":"Version restaurada exitosamente"}, status=status.HTTP_200_OK)
+    
--- historial/utils.py
+++ historial/utils.py
@@ -1,23 +1,49 @@
 def consultar_historial(model,object_id):
-    """consulta el historial completo de un objeto dado su ID"""
+    """
+    Consulta el historial completo de un objeto dado su ID
+    """
     return model.history.filter(id=object_id).order_by('-history_date')
 
-# def obtener_historial_id(model. object_id):
-#     """Devuelve el historial complet de un objeto con ID"""
-#     historial=consultar_historial(model, object_id)
-#     if not historial.exists():
-#         raise ValueError(f"No se encontro historial para el objeto con id {object_id}")
-#     return historial
+def obtener_historial_limitado(model, object_id, limit=5):
+    """
+    Obtiene y devuelve las ultimas 'limit' versiones del historial de un objeto.
+    - lanza un error si hay menos de dos versiones.
+    """
+    historial=consultar_historial(model, object_id)[:limit]
+    if len(historial)<2:
+        raise ValueError(f"No hay suficientes versiones para comparar.")
+    return historial
 
-def obtener_difernecia(version1, version2):
-    """compara dos versiones del historial y devueve la diferencia"""
+def comparar_versiones(version1, version2, attribute="all"):
+    """
+    Compara dos versiones consecutivas del historial:
+    - Si 'attribute' es "all", compara todos los campos del objeto.
+    - Si propocionas un 'attribute' solo comparara ese atributo.
+    """
     diferencias=version1.diff_against(version2).changes
-    return [
-        {"campo": cambio.field,
-        "antes": cambio.old,
-        "despues": cambio.new}
-        for cambio in diferencias  # Itera sobre 'changes' o la lista que tengas
+    # si el atributo es "all", no fitrara y devolvera todos los campos
+    if attribute != "all":
+        diferencias = [cambio for cambio in diferencias if cambio.field == attribute]
+    return[
+        {"campo": cambio.field,"antes":cambio.old, "despues":cambio.new}
+        for cambio in diferencias
     ]
-def restaurar_version(version):
-    """Restaura un objeto a una version anterior."""
-    version.instance.save()
+def compara_varias_versiones(model, object_id, attribute="all", limit=5):
+    """
+    Comparar varias versiones consecutivas del historial de un objeto.
+    - Si 'attribute' es "all", compara todas las versiones.
+    - Si propocionas un 'attribute' solo comparara ese atributo a lo largo de las versiones .
+    """
+    historial = obtener_historial_limitado(model, object_id, limit)
+    cambios = []
+
+    for i in range(len(historial)-1):
+
+        diferencias = comparar_versiones(historial[i], historial[i+1], attribute)
+        if diferencias:
+            cambios.append({
+                "version_id": historial[i].history_id,
+                "fecha": historial[i].history_date,
+                "cambios": diferencias
+            })   
+    return cambios
