d3fc186edd18f09a1aaa8b7a1a05df9259c89d83
Renato <renatodaniel.cb@gmail.com>
2024-10-11 17:20:49 -0500

    difunto cargado

--- difunto/base.py
+++ difunto/base.py
@@ -4,7 +4,7 @@ from django.db import models
 class BaseModelDifunto(models.Model):
     names = models.CharField( max_length=200, verbose_name='nombres')
     last_names = models.CharField( max_length=200, verbose_name='apellidos')
-    idNumber = models.CharField(max_length=10, verbose_name='cedula', null=True, blank=True)
+    idNumber = models.CharField(max_length=10, verbose_name='cedula', unique=True)
     loadDate = models.DateTimeField(auto_now_add=True, verbose_name='creacion')
     updateDate = models.DateTimeField(auto_now=True, verbose_name='actualizacion')
     description = models.TextField(max_length=300, blank=True, null=True, verbose_name='observaciones')
