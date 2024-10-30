f26328a67a87f6d66f1d5cf177abb2ad859b588b
Renato <renatodaniel.cb@gmail.com>
2024-10-30 03:09:38 -0500

    areglo bug

--- tumba/urls.py
+++ tumba/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .viewsets import TumbaViewSet, TumbaGraficaViewSet, LoteViewSet, LoteGraficaViewSet
+from .viewsets import TumbaViewSet, LoteViewSet
 
 
 router = DefaultRouter()
