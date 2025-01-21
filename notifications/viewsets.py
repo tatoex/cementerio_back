aa32f6bbbe3927cef741da755c3529e7762d0455
Renato <renatodaniel.cb@gmail.com>
2025-01-21 02:47:50 -0500

    cambio notificaciones

--- notifications/viewsets.py
+++ notifications/viewsets.py
@@ -3,16 +3,22 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
 from .models import Notification
 from .serializers import NotificationSerializer
 from .filters import NotificationFilter
 
-
+class PagionacionNoti(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 105
+    
 class NotificationViewSet(ModelViewSet):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = NotificationFilter
+    pagination_class = PagionacionNoti
 
     @action(detail=True, methods=['PATCH'], url_path='mark-as-attended')
     def mark_as_attended(self, request, pk=None):
